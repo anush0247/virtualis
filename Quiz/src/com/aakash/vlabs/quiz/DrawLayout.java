c57dd00207ab6c2076e497c70fa9bd04b1aa9cc0
anush0247 <anush0247@gmail.com>
2014-06-24 10:55:53 +0530

    Update Answers Class

--- Quiz/src/com/aakash/vlabs/quiz/DrawLayout.java
+++ Quiz/src/com/aakash/vlabs/quiz/DrawLayout.java
@@ -1,7 +1,22 @@
 package com.aakash.vlabs.quiz;
 
+import android.view.View;
+import android.widget.LinearLayout;
+
 public class DrawLayout {
 	
 	// input : Parsed Question String array 
 	// output : parse the Answer String and return a view and validation conditions 
+	
+	LinearLayout qun_layout;
+	View view;
+	
+	public DrawLayout(LinearLayout qun_layout, View view){
+		this.qun_layout = qun_layout;
+		this.view = view;
+	}
+	 
+	public void drawTF(LinearLayout qun_layout, View view, String trueAns, String feedback){
+		
+	}
 }
--- Quiz/src/com/aakash/vlabs/quiz/QuizStart.java
+++ Quiz/src/com/aakash/vlabs/quiz/QuizStart.java
@@ -6,12 +6,16 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class QuizStart extends Activity {
+import com.aakash.vlabs.quiz.ParseAnswer.McqOpts;
+import com.aakash.vlabs.quiz.Question.OnAnswered;
+
+public class QuizStart extends Activity implements OnAnswered{
 
 	Button prev,next;
 	int currnt_qn = 1,next_qn = 2,prev_qn = 1,total_qn=10;
@@ -20,6 +24,8 @@ public class QuizStart extends Activity {
 	FragmentTransaction fragmentTransaction;
 	
 	TextView qn_status;
+	
+	MyAns[] AllAns = null;
 	ArrayList<String> Qns = new ArrayList<String>();
 	
     @Override
@@ -28,11 +34,56 @@ public class QuizStart extends Activity {
         setContentView(R.layout.quiz_start);
         
         Qns = getIntent().getStringArrayListExtra("qn_array");
-       //for(int i = 0; i<Qns.size();i++){
-        //	Log.d("Received Question "+i, Qns.get(i));
-        //}
         
         total_qn = Qns.size();
+        AllAns = new MyAns[total_qn];
+        for(int i=0;i<total_qn;i++){
+        	ParseQuestion pQuestion = new ParseQuestion(Qns.get(i));
+        	AllAns[i] = new MyAns(i);
+        	AllAns[i].setQn(pQuestion.parts[1]);
+        	AllAns[i].setQnType(pQuestion.parts[3]);
+        	
+        	ParseAnswer pAns = new ParseAnswer(pQuestion.parts[2]);
+        	if(AllAns[i].getQnType().equals("Multiple")){
+        		ArrayList<McqOpts> mcq = pAns.parseMCQ();
+        		for(int i1 = 0;i1<mcq.size();i1++){
+        			if(mcq.get(i1).isAns){
+        				AllAns[i].setTruemulOptAns(mcq.get(i1).value);
+        				AllAns[i].setFeedback(mcq.get(i1).feedback);
+        			}
+        		}
+        	}
+        	else if(AllAns[i].getQnType().equals("Multiple_many") || AllAns[i].getQnType().equals("Short_Answer")){
+        		ArrayList<String> tmpStrAry = new ArrayList<String>();
+        		ArrayList<String> tmpFeedback = new ArrayList<String>();
+        		ArrayList<McqOpts> mcq = pAns.parseMCQ();
+        		for(int i1 = 0;i1<mcq.size();i1++){
+        			if(mcq.get(i1).isAns){
+        				tmpStrAry.add(mcq.get(i1).value);
+        				tmpFeedback.add(mcq.get(i1).feedback);
+        			}
+        		}
+        		AllAns[i].setTruemulManyAns(tmpStrAry);
+        		AllAns[i].setMulFeedback(tmpFeedback);
+        	}
+        	else if(AllAns[i].getQnType().equals("True_false")){
+        		String[] tfArr = pAns.parseTrueFalse();
+        		AllAns[i].setTrueAns(tfArr[0]);
+        		AllAns[i].setFeedback(tfArr[1]);
+        	}
+        	else if(AllAns[i].getQnType().equals("Numeric")){
+        		ArrayList<float[]> tmpFloatAry = pAns.parseNumeric();
+        		AllAns[i].setTrueNumeric(tmpFloatAry);
+        	}
+        	else if(AllAns[i].getQnType().equals("Matching")){
+        		ArrayList<String[]> tmpStringAry = pAns.parseMatching();
+        		AllAns[i].setTrueMatch(tmpStringAry);
+        	}
+        	else {
+        		Log.d("Error #"+i,"Unable to identify type of Question");
+        	}
+        }
+        
         qn_status = (TextView) findViewById(R.id.quiz_qno_total);
         qn_status.setText("Quiz Question : "+(currnt_qn)+" / "+total_qn);
         
@@ -40,6 +91,7 @@ public class QuizStart extends Activity {
 		fragmentTransaction = fragmentManager.beginTransaction();
 		Question currentQuestion = new Question();
 		currentQuestion.setGift_qn(Qns.get(currnt_qn-1));
+		currentQuestion.setSavedAns(AllAns[currnt_qn-1]);
 		fragmentTransaction.add(R.id.frag_question, currentQuestion);
 		fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
@@ -63,6 +115,7 @@ public class QuizStart extends Activity {
 				else {
 					nextQuestion.setId(prev_qn);
 					nextQuestion.setGift_qn(Qns.get(prev_qn-1));
+					nextQuestion.setSavedAns(AllAns[prev_qn-1]);
 					qn_status.setText("Quiz Question : "+(prev_qn)+" / "+total_qn);
 					next_qn = currnt_qn;
 					currnt_qn = prev_qn;
@@ -89,6 +142,7 @@ public class QuizStart extends Activity {
 				else {
 					nextQuestion.setId(next_qn);
 					nextQuestion.setGift_qn(Qns.get(next_qn-1));
+					nextQuestion.setSavedAns(AllAns[next_qn-1]);
 					qn_status.setText("Quiz Question : "+(next_qn)+" / "+total_qn);
 					prev_qn = currnt_qn;
 					currnt_qn = next_qn;
@@ -109,11 +163,16 @@ public class QuizStart extends Activity {
 	@Override
 	public void onBackPressed() {
 		// TODO Auto-generated method stub
-		//Toast.makeText(getApplicationContext(), "" + currnt_qn + "" + prev_qn + "" + next_qn, Toast.LENGTH_SHORT).show();
 		this.finish();
 		super.onBackPressed();
 		
 	}
-    
+
+	@Override
+	public void updateAns(int QnNo, MyAns ans) {
+		// TODO Auto-generated method stub
+		AllAns[QnNo] = ans;
+	}
+
     
 }
